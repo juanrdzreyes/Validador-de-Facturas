b5583b98b9447f9a4a15174702f0d35a7190130c
Juan Rodriguez Reyes <juan.rdz.157@gmail.com>
2020-01-22 23:01:08 -0600

    Initial Commit

---
+++ Validador de Facturas/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Validador de Facturas
+//
+//  Created by Juan Rodriguez Reyes on 22/01/20.
+//  Copyright © 2020 Juan Rodriguez Reyes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Validador de Facturas/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Validador de Facturas
+//
+//  Created by Juan Rodriguez Reyes on 22/01/20.
+//  Copyright © 2020 Juan Rodriguez Reyes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
